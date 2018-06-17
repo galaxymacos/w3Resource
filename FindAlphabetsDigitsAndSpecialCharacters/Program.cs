fb920003a6c64d2aaa745157de12bd218aaedba3
galaxy <galaxymacos@gmail.com>
2018-06-17 18:29:20 -0400

    + w3resource

---
+++ FindAlphabetsDigitsAndSpecialCharacters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindAlphabetsDigitsAndSpecialCharacters
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int countAlphabet = 0;
+            int countNumber = 0;
+            int countSpecial = 0;
+            foreach (var character in s)
+            {
+                if (character <= '9' && character >= '0')
+                {
+                    countNumber++;
+                }
+
+                else if (character <= 'z' && character >= 'a' || character <= 'Z' && character >= 'A')
+                {
+                    countAlphabet++;
+                }
+
+                else
+                {
+                    countSpecial++;
+                }
+            }
+
+            Console.WriteLine("total of alphabets in the string is " + countAlphabet);
+            Console.WriteLine("total of digits in the string is " + countNumber);
+            Console.WriteLine("total of special number in the string is " + countSpecial);
+        }
+    }
+}
---
+++ MaximumOccuringCharacterInString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximumOccuringCharacterInString
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Dictionary<char,int> dictionary = new Dictionary<char, int>();
+            int maximumNumber = 0;
+            char maximumChar = ' ';
+            foreach (char c in s)
+            {
+                if (!dictionary.ContainsKey(c))
+                {
+                    dictionary.Add(c,1);
+                }
+                else
+                {
+                    dictionary[c]++;
+                }
+
+                if (dictionary[c] <= maximumNumber) continue;
+                maximumNumber = dictionary[c];
+                maximumChar = c;
+            }
+
+            Console.WriteLine($"{maximumChar} occurs {dictionary[maximumChar]} times");
+        }
+    }
+}
