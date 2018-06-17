56eee26bb787e515b19df3be9b71a6fb9de328e9
galaxy <galaxymacos@gmail.com>
2018-06-17 16:24:21 -0400

    + w3resource

--- InputAndPrint/Program.cs
+++ InputAndPrint/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace InputAndPrint
+﻿using System;
+
+namespace InputAndPrint
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            string s = Console.ReadLine();
+            Console.WriteLine($"The string you entered is: {s}");
         }
     }
 }
